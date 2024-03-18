84d449c7c77839312ccf31006219b40e0d811bc1
brucemack <bruce@mackinnon.com>
2024-03-18 14:21:20 -0400

    TCP working

--- src/main.cpp
+++ src/main.cpp
@@ -84,6 +84,7 @@ static void sendTCPCloseRespToClient(Client& client, uint16_t clientId, uint8_t 
     int rc = write(client.fd, header, totalLen);
 }
 
+
 static void sendTCPSendRespToClient(Client& client, uint16_t clientId) {
     uint8_t header[8];
     uint16_t totalLen = 5;
